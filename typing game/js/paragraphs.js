46757ec50f9c86e27aef14be8d9c98785a7aac99
kes7364 <102892079+kes7364@users.noreply.github.com>
2022-05-07 15:27:47 +0900

    Add files via upload

--- typing game/js/paragraphs.js
+++ typing game/js/paragraphs.js
@@ -1,10 +1,12 @@
 const paragraphs = [
-    'Hello.',
-    "I'm Kim Eunsoo, dreaming of becoming a front-end.",
-    'Welcome to my portfolio. Web responsive UI/UX for interest in design I learned the web publisher course, and the front end that allows me to implement dynamic things on the web page',
-    'I got interested in development. At the front-end, where new technologies are constantly emerging,',
-    'To keep up with the latest trends, we will be a developer who can identify trends through search,',
-    'know the strengths and weaknesses of each technology, and learn and apply the functions you want right away.',
-    'You can see my hard work and the results of my hard work.',
+    'Hello.  Welcome to my portfolio.',
+    'I’m Kim Eunsu who dreaming of becoming a front-end.',
+    'I learned the web publisher course, because I wad interested in design.',
+    'The front end that allows me to implement dynamic things on the web page I got interested in development. The job of the front-end is constantly coming. up withe new technologies.,',
+
+    'I’ll follow this latest trend.',
+    'I’ll  learn the pros and cons of various skills through search.',
+    'I’ll become a developer who can learn and apply the functions I want right away.',
+    'You can see my hard work and the results of my  portfolio.',
     'Please relax and enjoy. Thank you. :)',
 ];
