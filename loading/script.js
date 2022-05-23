7481bc03b5755bb06c57e37a87686774a8a289b2
kes7364 <102892079+kes7364@users.noreply.github.com>
2022-05-24 08:50:19 +0900

    Add files via upload

---
+++ loading/script.js
@@ -0,0 +1,13 @@
+CSS.registerProperty({
+    name: '--p',
+    syntax: '<number>',
+    initialValue: 1,
+    inherits: false,
+});
+
+CSS.registerProperty({
+    name: '--q',
+    syntax: '<number>',
+    initialValue: 0,
+    inherits: false,
+});
